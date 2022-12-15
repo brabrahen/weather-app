6c79280756f7b769ecb03a575f47dad1ce308221
Brayan Henrique <89050408+brabrahen@users.noreply.github.com>
2022-12-15 20:44:57 -0300

    Add files via upload

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+console.log("Client side javascript file is loaded!");
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+
+messageOne.textContent = 'From favascript'
+messageTwo.textContent = ''
+
+weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+
+    fetch("http://localhost:3000/weather?address=" + location).then(
+        (response) => {
+            response.json().then((data) => {
+                if (data.error) {
+                    console.log(data.error);
+                } else {
+                    console.log(data.location);
+                    console.log(data.forecast);
+                }
+            });
+        }
+    );
+});
+
+//pasta 8 aula 6
